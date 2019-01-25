459dcb22fe662c48fb15b262fd8c04a951403f72
sespinosav <44402775+sespinosav@users.noreply.github.com>
2019-01-25 11:06:37 -0500

    Update GraphAL.py

--- talleres/taller01/GraphAL.py
+++ talleres/taller01/GraphAL.py
@@ -1,26 +1,29 @@
 class GraphAL:
 
     #La implementacion que haremos
-    #se basa en una lista de tuplas
-    #de diccionarios
-    #donde el indice de la lista de las tuplas
-    #es el inicio 
+    #se basa en una lista de diccionarios
+    #donde el indice de la lista de los diccionarios
+    #es el inicio
     #la llave del diccionario es el destino
-    #y el peso es el valor del diccionario
-
+    #y el peso es el valor de la llave en cada diccionario
+    
     def __init__(self, size):
+        
+      #Creamos la lista
       self.__listt = []
-
+       
+      #Llenamos la lista con diccionarios vacios
       for i in range(size):
         self.__listt.append(dict())
       
     def addArc(self, vertex, edge, weight):
-
+        
+      #Agregamos una clave valor al diccionario ya existente
       self.__listt[vertex].update({edge : weight})
 
 
     def getSuccessors(self, vertice):
-
+      
       return self.__listt[vertice].keys()
 
         
