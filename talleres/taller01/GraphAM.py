dbd114422a7d5adfbefeac3a76b5b5a8e978984d
sespinosav <44402775+sespinosav@users.noreply.github.com>
2019-01-25 22:02:43 -0500

    Documentacion correspondiente

--- talleres/taller01/GraphAM.py
+++ talleres/taller01/GraphAM.py
@@ -1,5 +1,7 @@
 class GraphAM:
-
+    
+    """Implementacion de grafos utilizando matrices de adjacencia"""
+    #@paran tamaño de la matriz nxn
     def __init__(self, size):
 
       #Creamos la matriz
@@ -7,20 +9,21 @@ class GraphAM:
       for i in range(size):
         self.__matriz[i] = [None] * size
     
-      #Inicializamos todos los pesos en None
-      #esto provoca que si dos vertices
-      #no estan conectados tendran peso None
-      #e identificaremos que no tienen arco
-      #cuando se agrega un arco, al tener que pasar
-      #el peso por parametro
-      #identificamos que hay arco
+      """Inicializamos todos los pesos en None
+      esto provoca que si dos vertices
+      no estan conectados tendran peso None
+      e identificaremos que no tienen arco
+      cuando se agrega un arco, al tener que pasar
+      el peso por parametro
+      identificamos que hay arco"""
       for i in self.__matriz:
         for j in i:
           j = None
 
-    #Retorna una lista con diccionarios
-    #las llaves de los diccionarios son el indice
-    #y la clave es el diccionario
+    """@return listt una lista con diccionarios
+    de los arcos totales del grafo
+    las llaves de los diccionarios son el inicie(source)
+    y la clave es el destino"""
     def getEdges(self):
       listt = []
       for i in self.__matriz:
@@ -30,7 +33,10 @@ class GraphAM:
             listt.append(dicc)
 
       return listt
-      
+
+    """@param source nodo de partido
+    @param destination hacia donde va el arco
+    @return weight el peso de la longitud entre source y destination"""   
     def getWeight(self, source, destination):
 
       #Vamos a la poscision de la matriz en
@@ -38,15 +44,16 @@ class GraphAM:
       #Y tomamos su peso
       return self.__matriz[source][destination]
 
-    #Vamos a la poscision de la matriz en
-    #El nodo de partida y el destinatino
-    #Y le agregamos un peso
-    #Lo que provoca que cambie de estar en None
-    #A tener un peso y así sabremos que existe el arco
+    """Vamos a la poscision de la matriz en
+    El nodo de partida y el destinatino
+    Y le agregamos un peso
+    Lo que provoca que cambie de estar en None
+    A tener un peso y así sabremos que existe el arco"""
     def addArc(self, source, destination, weight):
       self.__matriz[source][destination] = weight 
     
-
+    #@param vertex vertice del cual vamos a sacar sus sucesroes
+    #@return succesirs una lista de los nodos succesores de vertex
     def getSuccessors(self, vertex):
 
       #Inicializamos la lista que deseamos retornar
