bd1381cf9c371df285341d8279f11f1f4ffd2209
Santiago Espinosa Valderrama <44402775+sespinosav@users.noreply.github.com>
2019-01-30 13:32:07 -0500

    Update Doc

--- talleres/taller01/GraphAL.py
+++ talleres/taller01/GraphAL.py
@@ -7,40 +7,52 @@ class GraphAL:
     la llave del diccionario es el destino
     y el peso es el valor de la llave en cada diccionario"""
     
-    #Constructor para el grafo dirigido
-    #@param tamaño de la matriz de orden nxn
     def __init__(self, size):
+    
+    """
+    Constructor para el grafo dirigido
+    :param size: tamaño de la matriz de orden nxn
+    """
         
       #Creamos la lista
       self.__listt = []
        
-      #Llenamos la lista con diccionarios vacio
-      #@param tamaño de la matriz nxn
       for i in range(size):
+       #Llenamos la lista con diccionarios vacio
         self.__listt.append(dict())
       
-    """@param vertex desde donde se hara el arco
-       @param destination hacia donde va el arco
-       @param weight el peso de la longitud entre source y destination"""
     def addArc(self, vertex, edge, weight):
         
-      """Agregamos una clave valor al diccionario ya existente
-      Que sera el inicio y el destino que conforma al arco nuevo"""
-      #@see <https://www.programiz.com/python-programming/methods/dictionary/update> ver documentacion de la funcion update
+    """
+    :param vertex: desde donde se hara el arco
+    :param edge: hacia donde va el arco
+    :param weight: el peso de la longitud entre source y destination
+    """
+        
+      """
+      Agregamos una clave valor al diccionario ya existente
+      Que sera el inicio y el destino que conforma al arco nuevo
+      #see <https://www.programiz.com/python-programming/methods/dictionary/update> ver documentacion de la funcion update
+      """
       self.__listt[vertex].update({edge : weight})
 
-    #@param vertex desde donde se hara el arco
-    def getSuccessors(self, vertice):
-      
-      #@return una lista con las llaves de la lista
-      #del diccionario, que vendrian siendo los nodos succesores
+     def getSuccessors(self, vertice):
+        
+     """
+     param vertex desde donde se hara el arco     
+     :return: una lista con las llaves de la lista del diccionario, que vendrian siendo los nodos succesores
+     """
+    
       return self.__listt[vertice].keys()
-
-    #@param source desde donde se inicia el arco
-    #@param destination desde donde se hara el arco
-    def getWeight(self, source, destination):
-       
-      #@return el peso del arco entre el inicio y el destino
+    
+    def getWeight(self, source, destination
+                  
+    """
+    :param source: desde donde se inicia el arco
+    :param destination: desde donde se hara el arco
+    :return: el peso del arco entre el inicio y el destino
+    """
+    
       return self.__listt[source][destination]
 
     def __str__(self):
