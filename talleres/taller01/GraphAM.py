488bd535ff54cf437d71b3197b37b9ccc0ee6b8e
Santiago Espinosa Valderrama <44402775+sespinosav@users.noreply.github.com>
2019-01-30 13:38:24 -0500

    Update Doc

--- talleres/taller01/GraphAM.py
+++ talleres/taller01/GraphAM.py
@@ -1,30 +1,39 @@
 class GraphAM:
     
-    """Implementacion de grafos utilizando matrices de adjacencia"""
-    #@paran tamaño de la matriz nxn
+    #Implementacion de grafos utilizando matrices de adjacencia
+    
     def __init__(self, size):
-
+        
+      #:param size: tamaño de la matriz nxn
       #Creamos la matriz
+        
       self.__matriz = [None] * size
       for i in range(size):
         self.__matriz[i] = [None] * size
     
-      """Inicializamos todos los pesos en None
+      """
+      Inicializamos todos los pesos en None
       esto provoca que si dos vertices
       no estan conectados tendran peso None
       e identificaremos que no tienen arco
       cuando se agrega un arco, al tener que pasar
       el peso por parametro
-      identificamos que hay arco"""
+      identificamos que hay arco
+      """
       for i in self.__matriz:
         for j in i:
           j = None
 
-    """@return listt una lista con diccionarios
+    def getEdges(self):
+        
+        
+    """
+    :return listt: una lista con diccionarios
     de los arcos totales del grafo
     las llaves de los diccionarios son el inicie(source)
-    y la clave es el destino"""
-    def getEdges(self):
+    y la clave es el destino
+    """
+    
       listt = []
       for i in self.__matriz:
         for j in i:
@@ -34,27 +43,38 @@ class GraphAM:
 
       return listt
 
-    """@param source nodo de partido
-    @param destination hacia donde va el arco
-    @return weight el peso de la longitud entre source y destination"""   
+    """
+    :param source: nodo de partido
+    :param destination: hacia donde va el arco
+    :return weight: el peso de la longitud entre source y destination"""   
+    
     def getWeight(self, source, destination):
-
+        
+     """
+    :param source: nodo de partido
+    :param destination: hacia donde va el arco
+    :return weight: el peso de la longitud entre source y destination
+    """   
+    
       #Vamos a la poscision de la matriz en
       #El nodo de partida y el destinatino
       #Y tomamos su peso
       return self.__matriz[source][destination]
 
-    """Vamos a la poscision de la matriz en
+    """
+    Vamos a la poscision de la matriz en
     El nodo de partida y el destinatino
     Y le agregamos un peso
     Lo que provoca que cambie de estar en None
-    A tener un peso y así sabremos que existe el arco"""
+    A tener un peso y así sabremos que existe el arco
+    """
     def addArc(self, source, destination, weight):
       self.__matriz[source][destination] = weight 
     
-    #@param vertex vertice del cual vamos a sacar sus sucesroes
-    #@return succesirs una lista de los nodos succesores de vertex
     def getSuccessors(self, vertex):
+     
+    #:param vertex: vertice del cual vamos a sacar sus sucesroes
+    #:return succesirs: una lista de los nodos succesores de vertex
 
       #Inicializamos la lista que deseamos retornar
       succesor = []
