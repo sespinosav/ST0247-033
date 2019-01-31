c175757995d0cd9b454631650143415fef7360a4
Santiago Espinosa Valderrama <44402775+sespinosav@users.noreply.github.com>
2019-01-31 14:56:43 -0500

    Pruebas
    Pruebas de implementacion de fuerza bruta

--- talleres/taller02/main.py
+++ talleres/taller02/main.py
@@ -3,11 +3,21 @@ from Bfa import *
 """
 Pruebas de implementacion de BFA
 """
+test = Bfa()
 
 conjunto = "abcd"
-test = Bfa()
 test.subConjuntos(conjunto)
 
 print()
 
 test.permutaciones(conjunto)
+
+print()
+
+conjunto = "melo"
+test.subConjuntos(conjunto)
+
+print()
+
+test.permutaciones(conjunto)
+
